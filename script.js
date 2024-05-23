caf07908010d8ba566e3a5274d07016b7ae38196
Rohith <rohithofficial10@gmail.com>
2024-05-23 06:08:37 +0530

    add winnning and losing messages

--- script.js
+++ script.js
@@ -15,27 +15,47 @@ function random() {
 function play() {
     const ch = random();
     const sel = selected();
+    var msg = "Computer chose "+ch+". ";
     document.getElementById("msg").textContent = "Computer chose "+ch;
     if (sel == "Rock") {
-        if (ch == "Paper") comp++;
-        else if (ch == "Scissors") you++;
+        if (ch == "Paper") {
+            comp++;
+            msg += "You lose!";
+        }
+        else if (ch == "Scissors") { 
+            you++;
+            msg += "You win!";
+        }
         document.getElementById("rock").checked = false;
     }
     else if (sel == "Paper") {
-        if (ch == "Rock") you++;
-        else if (ch=="Scissors") comp++;
+        if (ch == "Rock") {
+            you++;
+            msg += "You win!";
+        }
+        else if (ch=="Scissors") {
+            comp++;
+            msg += "You lost!";
+        }
         document.getElementById("paper").checked = false;
     }
     else if (sel == "Scissors") {
-        if (ch=="Rock") comp++;
-        else if (ch=="Paper") you++;
+        if (ch=="Rock") {
+            comp++;
+            msg += "You lose!";
+        }
+        else if (ch=="Paper") {
+            you++;
+            msg += "You win!";
+        }
         document.getElementById("scissors").checked = false;
     }
     else {
-        document.getElementById("msg").textContent = "Choose an option pls!";
+        msg = "Choose an option pls!";
     }
     document.getElementById("info1").textContent = you;
     document.getElementById("info2").textContent = comp;
+    document.getElementById("msg").textContent = msg;
 }
 var you = 0;
 var comp = 0;
